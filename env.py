dc150092e736e20fcc7d0d12bb99e0dca99541d9
Matija Basanovic <mbasanov@gmail.com>
2020-02-21 10:33:16 +0000

    Edit gitignore

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ.setdefault("SECRET_KEY", "'_wv(pbehoe0gepl__)9b54@g!44i3v0bdq0vg+mafd5)9e*x(w'")
-
-os.environ.setdefault("DATABASE_URL", "postgres://onfmwpzarbfprq:0cf04f3df5e8435fee78e12f4890fbf8739b0c9708d7f737838941f97042f994@ec2-54-246-90-10.eu-west-1.compute.amazonaws.com:5432/dd7n2206tb0m8j")
